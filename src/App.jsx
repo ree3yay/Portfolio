345671d087aa40e742cda1d04346e7665f9a9309
John Doe <johndoe@email.com>
2024-01-04 00:11:49 +0800

    Styles Initiated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.scss"
+
+const App = () => {
+return <div>
+
+<section>Hero</section>
+<section>Paralax</section>
+<section>Services</section>
+<section>Paralax</section>
+<section>Portfolio1</section>
+<section>Portfolio2</section>
+<section>Portfolio3</section>
+<section>Contact</section>
+
+ </div>
+ 
+};
+
+export default App;
